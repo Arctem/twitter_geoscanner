9c9e17b0b0b0c6ec6dda31aac1f015fde0c4cafd
Benjamin Bean <gladclef@gmail.com>
2014-10-09 02:49:24 -0600

    added way of computing necessary db space

---
+++ tweet_count.py
@@ -0,0 +1,34 @@
+tweets_per_second = 8
+seconds_per_day = 60 * 60 * 24
+num_days = 20
+
+time = 3
+lat = 4
+lon = 4
+hashtag_id_size = 3
+num_hashtags_per_tweet = 5
+
+avg_hashtag_length = 25
+avg_unique_hashtags_per_tweet = 0.5
+
+def getTweetSizeInBytes():
+    return time + lat + lon + (hashtag_id_size * num_hashtags_per_tweet)
+
+def getTotalTweetCount():
+    return tweets_per_second * seconds_per_day * num_days
+
+def getTotalTweetSize():
+    return getTotalTweetCount() * getTweetSizeInBytes()
+
+def getHashtagMapSize():
+    return avg_unique_hashtags_per_tweet * (avg_hashtag_length + 1) * getTotalTweetCount()
+
+def getTotalSize():
+    return getTotalTweetSize() + getHashtagMapSize()
+
+# get the total tweet size in megabytes
+def main():
+    print getTotalSize() / 1024.0 / 1024.0
+
+if __name__ == "__main__":
+    main()
