7dfe43465368eeeb3a8510af505c7d81020e36bb
Russell White <rarctem@gmail.com>
2015-05-06 23:01:58 -0400

    Make things work properly with sqlalchemy. Just get all tweets with geo data, it seems to be the largest set I can find.

--- tweetest.py
+++ tweetest.py
@@ -5,14 +5,34 @@ import time
 import re
 from datetime import datetime
 import sys
+import ConfigParser
 
 import tweepy
 from tweepy import StreamListener
 import nltk.data
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey
+from sqlalchemy import Sequence
 
-import db
+conf = ConfigParser.RawConfigParser()
+conf.read('mysql.conf')
+user = conf.get('db', 'user')
+password = conf.get('db', 'password')
+host = conf.get('db', 'host')
+database = conf.get('db', 'database')
 
-DEBUG = True
+#engine = create_engine('sqlite:///:memory:', echo=True)
+#engine = create_engine('mysql://{user}:{password}@{host}/{database}'.format(user=user, password=password, host=host, database=database))
+engine = create_engine('sqlite:///twitter.db')
+Base = declarative_base()
+Session = sessionmaker()
+
+DEBUG = False
 
 consumer_key = open('cons_key.txt', 'r').read().strip()
 consumer_secret = open('cons_secret.txt', 'r').read().strip()
@@ -26,15 +46,30 @@ classifier = nltk.data.load('classifiers/movie_reviews_NaiveBayes.pickle')
 EXITAFTERTHREEMINUTES = False
 init_time = datetime.now()
 
+class Tweet(Base):
+  __tablename__ = 'tweets'
+
+  id = Column(Integer, Sequence('tweet_id_sequence'), primary_key=True)
+  text = Column(String)
+  sentiment = Column(String)
+  date = Column(DateTime, default=datetime.utcnow)
+
+class Hash(Base):
+  __tablename__ = 'hash'
+
+  id = Column(Integer, Sequence('hash_id_sequence'), primary_key=True)
+  tag = Column(String)
+  tweet_id = Column(Integer, ForeignKey('tweets.id'))
+  tweet = relationship("Tweet", backref=backref('hashes', order_by=id))
+
 class DataStreamer(StreamListener):
-  def __init__(self, api = None, fprefix = 'streamer', conn = None):
+  def __init__(self, api = None, fprefix = 'streamer'):
     self.api = api or API()
     self.counter = 0
     self.hash_counter = 0
     self.fprefix = fprefix
     self.start = time.clock()
     self.hashes = {}
-    self.conn = conn
 
   def on_status(self, data):
     if EXITAFTERTHREEMINUTES:
@@ -42,18 +77,15 @@ class DataStreamer(StreamListener):
         quit()
     
     text = dict([(word, True) for word in data.text.split()])
-    print('{}: {}'.format(dir(classifier.classify(text)), repr(data.text)))
-    return
+    classification = classifier.classify(text)
+    print('{}: {}'.format(classification, repr(data.text)))
     
     if self.counter == 0:
       print('DataStreamer Starting')
     self.counter += 1
     #print(self.counter)
-    if self.counter % 100 == 0:
+    if self.counter % 10 == 0:
       print('{} - {} tweets per second'.format(self.counter, self.counter / (time.clock() - self.start) / 60))
-
-    if self.hash_counter % 100 == 0:
-      print('{} - {} hashed tweets per second'.format(self.hash_counter, self.hash_counter / (time.clock() - self.start) / 60))
 
     hashtags = rehash.findall(data.text)
     hashtags_list = []
@@ -62,13 +94,15 @@ class DataStreamer(StreamListener):
       if len(hashtag) > 0:
         hashtags_list.append(hashtag)
 
-    if len(hashtags_list) > 0 and self.conn:
-      for hashtag in hashtags_list:
-        hash_occurances = self.conn.increaseHashtagCount(hashtag)
-        if hash_occurances % 10 == 0:
-          print('{} has occured {} times.'.format(hashtag, hash_occurances))
-      geo = data.geo["coordinates"]
-      self.conn.insertTweet({"geo": {"lat":geo[0],"lon":geo[1]}, "tags": hashtags_list})
+    session = Session()
+    tweet = Tweet(text = data.text, sentiment=classification)
+    session.add(tweet)
+    for hashtag in hashtags_list:
+      #hash_occurances = self.conn.increaseHashtagCount(hashtag)
+      h = Hash(tag = hashtag, tweet = tweet)
+      session.add(h)
+      # if hash_occurances % 10 == 0:
+      #   print('{} has occured {} times.'.format(hashtag, hash_occurances))
     
     if DEBUG:
       print("=" * 40)
@@ -76,10 +110,10 @@ class DataStreamer(StreamListener):
       #print(dir(data.author))
       print(data.text)
       #print(data.geo, data.coordinates)
-      print(u'{}, located at {}, said "{}".'.format(data.author.name,
-        parse_geo(data.geo), data.text))
+      print(u'{} said "{}".'.format(data.author.name, data.text))
       #return not data.geo or not data.coordinates
-
+    
+    session.commit()
     return True
 
   def on_limit(self, track):
@@ -93,13 +127,14 @@ def main():
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token_key, access_token_secret)
 
-  connection = db.Connection()
+  Base.metadata.create_all(engine)
+  Session.configure(bind=engine)
 
   api = tweepy.API(auth)
-  l = DataStreamer(api, conn=connection)
+  l = DataStreamer(api)
   stream = tweepy.Stream(auth, l)
-  #stream.filter(locations=[-180,-90,180,90], stall_warnings=True)
-  stream.filter(track=['#obama'], stall_warnings=True)
+  stream.filter(locations=[-180,-90,180,90], stall_warnings=True)
+  #stream.filter(track=['#obama'], stall_warnings=True)
 
 if __name__ == '__main__':
   main()
